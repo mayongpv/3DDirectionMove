e1251015de7da2867a37e5b8f7b0303473d43152
mayongpv <82883541+mayongpv@users.noreply.github.com>
2021-06-21 20:11:20 +0900

    foreach문 추가, 복붙 - 어택중에는 이동이나 아이들 모션 하지 않게함, 어택, 아이들, 런 애니메이션 클립 이름 변경

--- Assets/DirectionMove.cs
+++ Assets/DirectionMove.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,38 +11,82 @@ public class DirectionMove : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         lastMoveDirection = transform.forward;
+        lastMoveDirection = transform.forward;
 
         //aimationLength 초기화.
+        foreach (var animationClip in animator.runtimeAnimatorController.animationClips)
+        {
+            aimationLength[animationClip.name] = animationClip.length; // aimationLength["Attack"] = 1.4;
+        }
     }
 
-    Dictionary<string, float> aimationLength = null;
+    Dictionary<string, float> aimationLength = new Dictionary<string, float>();
 
     void Update()
     {
-        Vector3 move = Vector3.zero;
-        if (Input.GetKey(KeyCode.A)) move.z = -1;
-        if (Input.GetKey(KeyCode.D)) move.z = 1;
-        if (Input.GetKey(KeyCode.S)) move.x = -1;
-        if (Input.GetKey(KeyCode.W)) move.x = 1;
 
-        if (move != Vector3.zero)
+        void Update()
         {
-            move.Normalize();
-            transform.Translate(move * 5 * Time.deltaTime
-                , Space.World);
+            Vector3 move = Vector3.zero; // move = new Vector3(0, 0, 0)
 
-            //transform.forward = move; // 이코드 있으면 작동안함.
-            lastMoveDirection = move;
-            state = StateType.Run;
-            animator.Play("Run");
+            // 어택을 했으면 어택 애니메이션 진행중인 동안은 
+            // Run과 Idle을 하지 않게 하자
+            // 각 애니메이션의 길이가 필요하다.
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //어택에니메이션이 끝났을때 state = None
+                StartCoroutine(AttackCo());
+            }
+
+            if (state != StateType.Attack)
+            {
+                move = MoveAndIdle(move);
+            }
+
+            transform.forward = Vector3.Slerp(transform.forward
+                , move, roateLerp);
         }
-        else
+
+        IEnumerator AttackCo()
         {
-            move = lastMoveDirection;
-            state = StateType.Idle;
-            animator.Play("Idle");
-        };
-        return move;
+            state = StateType.Attack;
+            animator.Play("Attack");
+            float attackAnimationTime = aimationLength["Attack"];
+            yield return new WaitForSeconds(attackAnimationTime);
+            state = StateType.None;
+        }
+
+        /// <summary>
+        /// Move/Idle애니메이션, 실제 이동
+        /// </summary>
+        /// <param name="move"></param>
+        /// <returns></returns>
+        Vector3 MoveAndIdle(Vector3 move)
+        {
+            if (Input.GetKey(KeyCode.A)) move.x = -1;
+            if (Input.GetKey(KeyCode.D)) move.x = 1;
+            if (Input.GetKey(KeyCode.W)) move.z = 1;
+            if (Input.GetKey(KeyCode.S)) move.z = -1;
+
+            if (move != Vector3.zero)
+            {
+                move.Normalize();
+                transform.Translate(move * 5 * Time.deltaTime
+                    , Space.World);
+
+                //transform.forward = move; // 이코드 있으면 작동안함.
+                lastMoveDirection = move;
+                state = StateType.Run;
+                animator.Play("Run");
+            }
+            else
+            {
+                move = lastMoveDirection;
+                state = StateType.Idle;
+                animator.Play("Idle");
+            };
+            return move;
+        }
     }
     public float speed = 5;
     public float roateLerp = 0.5f;
