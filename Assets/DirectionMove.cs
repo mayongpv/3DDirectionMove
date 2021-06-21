953c0498d2728d233e0b1ac17ff8e0f1be332d86
mayongpv <82883541+mayongpv@users.noreply.github.com>
2021-06-21 17:53:13 +0900

    Lerp 적용 - 움직일 떄 부드럽게 회전

--- Assets/DirectionMove.cs
+++ Assets/DirectionMove.cs
@@ -8,6 +8,7 @@ public class DirectionMove : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
+        lastMoveDirection = transform.forward;
     }
     void Update()
     {
@@ -15,7 +16,7 @@ public class DirectionMove : MonoBehaviour
         if (Input.GetKey(KeyCode.A)) move.z = -1;
         if (Input.GetKey(KeyCode.D)) move.z = 1;
         if (Input.GetKey(KeyCode.S)) move.x = -1;
-        if (Input.GetKey(KeyCode.W)) move.x = -1;
+        if (Input.GetKey(KeyCode.W)) move.x = 1;
 
         if (move != Vector3.zero)
         {
@@ -23,8 +24,31 @@ public class DirectionMove : MonoBehaviour
             transform.Translate(move * 5 * Time.deltaTime, Space.World);
 
             transform.forward = move;
+            state = StateType.Run;
+            animator.Play("Run");
         }
+        else
+        {
+            lastMoveDirection = move;
+            state = StateType.Idle;
+            animator.Play("Idle");
+        }
+
+        //회전 부드럽게 하는 것
+        transform.forward = Vector3.Slerp(transform.forward, move, roateLerp);
+
     }
     public float speed = 5; //1초당 유닛 : 1초당 5유닛을 간다.
+    public float roateLerp = 0.5f;
+        public Vector3 lastMoveDirection ;
+
+    public StateType state = StateType.Idle;
+    public enum StateType
+    {
+    Idle,
+    Run,
+    Jump,
+    Attack,
+    }
 
 }
