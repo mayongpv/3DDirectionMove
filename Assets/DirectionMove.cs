d9452aaabe269145f8d8c20172703299dabf84fa
mayongpv <82883541+mayongpv@users.noreply.github.com>
2021-06-21 22:10:01 +0900

    뭐지?

--- Assets/DirectionMove.cs
+++ Assets/DirectionMove.cs
@@ -1,7 +1,8 @@
-﻿
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class DirectionMove : MonoBehaviour
 {
@@ -11,94 +12,125 @@ public class DirectionMove : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         lastMoveDirection = transform.forward;
-        lastMoveDirection = transform.forward;
 
-        //aimationLength 초기화.
-        foreach (var animationClip in animator.runtimeAnimatorController.animationClips)
-        {
-            aimationLength[animationClip.name] = animationClip.length; // aimationLength["Attack"] = 1.4;
-        }
+        //    aimationLength 초기화.
+        //    foreach (var animationClip in animator.runtimeAnimatorController.animationClips)
+        //    {
+        //        aimationLength[animationClip.name] = animationClip.length; // aimationLength["Attack"] = 1.4;
+        //    }
+        //}
     }
-
-    Dictionary<string, float> aimationLength = new Dictionary<string, float>();
+    //Dictionary<string, float> aimationLength = new Dictionary<string, float>();
 
     void Update()
     {
+        Vector3 move = Vector3.zero; // move = new Vector3(0, 0, 0)
 
-        void Update()
+        // 어택을 했으면 어택 애니메이션 진행중인 동안은 
+        // Run과 Idle을 하지 않게 하자
+        // 각 애니메이션의 길이가 필요하다.
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Vector3 move = Vector3.zero; // move = new Vector3(0, 0, 0)
-
-            // 어택을 했으면 어택 애니메이션 진행중인 동안은 
-            // Run과 Idle을 하지 않게 하자
-            // 각 애니메이션의 길이가 필요하다.
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                //어택에니메이션이 끝났을때 state = None
-                StartCoroutine(AttackCo());
-            }
-
-            if (state != StateType.Attack)
-            {
-                move = MoveAndIdle(move);
-            }
-
-            transform.forward = Vector3.Slerp(transform.forward
-                , move, roateLerp);
+            //어택에니메이션이 끝났을때 state = None
+            StartCoroutine(AttackCo());
         }
 
-        IEnumerator AttackCo()
+        if (State != StateType.Attack)
         {
-            state = StateType.Attack;
-            animator.Play("Attack");
-            float attackAnimationTime = aimationLength["Attack"];
-            yield return new WaitForSeconds(attackAnimationTime);
-            state = StateType.None;
+            move = MoveAndIdle(move);
         }
 
-        /// <summary>
-        /// Move/Idle애니메이션, 실제 이동
-        /// </summary>
-        /// <param name="move"></param>
-        /// <returns></returns>
-        Vector3 MoveAndIdle(Vector3 move)
-        {
-            if (Input.GetKey(KeyCode.A)) move.x = -1;
-            if (Input.GetKey(KeyCode.D)) move.x = 1;
-            if (Input.GetKey(KeyCode.W)) move.z = 1;
-            if (Input.GetKey(KeyCode.S)) move.z = -1;
-
-            if (move != Vector3.zero)
-            {
-                move.Normalize();
-                transform.Translate(move * 5 * Time.deltaTime
-                    , Space.World);
-
-                //transform.forward = move; // 이코드 있으면 작동안함.
-                lastMoveDirection = move;
-                state = StateType.Run;
-                animator.Play("Run");
-            }
-            else
-            {
-                move = lastMoveDirection;
-                state = StateType.Idle;
-                animator.Play("Idle");
-            };
-            return move;
-        }
+        transform.forward = Vector3.Slerp(transform.forward
+            , move, roateLerp);
     }
-    public float speed = 5;
-    public float roateLerp = 0.5f;
-    public Vector3 lastMoveDirection;
 
-    public StateType state = StateType.Idle;
-    public enum StateType
+    //실제 애니메니션 길이보다 먼저 움직이게 하자
+    public float attckAnimationWaitTime = 0.9f;
+    IEnumerator AttackCo()
     {
-        None,  // 아무것도 안하는 상태, 어택이 끝나면 되는 상태
-        Idle,  // 가만있을때,...
-        Run,
-        Jump,
-        Attack,
+        State = StateType.Attack;
+        //animator.Play("Attack");
+        yield return null;
+        float attackAnimationTime = GetCurrentAimationTime();
+        attackAnimationTime = attackAnimationTime * attackAnimationWaitTime;
+        yield return new WaitForSeconds(attackAnimationTime);
+        state = StateType.None;
+    }
+
+    private float GetCurrentAnimationTime();
+{
+    var state = animator.GetCurrentAnimatorStateInfo(0);
+    return state.length;
+}
+
+
+// < summary >
+// Move / Idle애니메이션, 실제 이동
+// </summary>
+// <param name="move"></param>
+// <returns></returns>
+Vector3 MoveAndIdle(Vector3 move)
+{
+    if (Input.GetKey(KeyCode.A)) move.x = -1;
+    if (Input.GetKey(KeyCode.D)) move.x = 1;
+    if (Input.GetKey(KeyCode.W)) move.z = 1;
+    if (Input.GetKey(KeyCode.S)) move.z = -1;
+
+    if (move != Vector3.zero)
+    {
+        move.Normalize();
+        transform.Translate(move * 5 * Time.deltaTime
+            , Space.World);
+
+        //transform.forward = move; // 이코드 있으면 작동안함.
+        lastMoveDirection = move;
+        state = StateType.Run;
+    }
+    else
+    {
+        move = lastMoveDirection;
+        state = StateType.Idle;
+    };
+    return move;
+}
+public float speed = 5;
+public float roateLerp = 0.5f;
+public Vector3 lastMoveDirection;
+
+public StateType State
+{
+    get { return state; }
+    set
+    {
+        // 쉬는 시간에 추가함.
+        if (state == value)
+            return;
+        // 쉬는 시간에 추가함.끝
+
+        state = value;
+
+        var animationInfo = blendingInfos.Find(x => x.state == state);
+        if (animationInfo != null)
+            animator.CrossFade(animationInfo.clipName, animationInfo.time);
     }
 }
+public StateType state = StateType.Idle;
+
+public enum StateType
+{
+    None,  // 아무것도 안하는 상태, 어택이 끝나면 되는 상태
+    Idle,  // 가만있을때,...
+    Run,
+    Jump,
+    Attack,
+}
+public List<BlendingInfo> blendingInfos;
+
+[System.Serializable] //인스펙터 아래 클래스를 노출시키자
+public class BlendingInfo
+{
+    public StateType state;
+    public string clipName;
+    public float time;
+}
+
