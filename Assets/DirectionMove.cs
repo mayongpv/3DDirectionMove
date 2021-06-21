30cf524bab97d2d0f5be4014cafa0c5821937c65
mayongpv <82883541+mayongpv@users.noreply.github.com>
2021-06-21 17:26:11 +0900

    초기 커밋.

---
+++ Assets/DirectionMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionMove : MonoBehaviour
+{
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        Vector3 move = Vector3.zero;
+        if (Input.GetKey(KeyCode.A)) move.z = -1;
+        if (Input.GetKey(KeyCode.D)) move.z = 1;
+        if (Input.GetKey(KeyCode.S)) move.x = -1;
+        if (Input.GetKey(KeyCode.W)) move.x = -1;
+
+        if (move == Vector3.zero)
+        {
+            move.Normalize();
+            transform.Translate(move * 5 * Time.deltaTime);
+
+            transform.forward = move;
+        }
+    }
+    public float speed = 5; //1초당 유닛 : 1초당 5유닛을 간다.
+
+}
